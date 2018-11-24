1c9d60beb4cf2dc39ae80255897df5fb9929f750
Alexander Amundson <alex.amundson987@gmail.com>
2018-11-24 11:27:03 -0800

    Adding return largest numbers

---
+++ FCC tutorial/Basic Algorithms/5Return_largest_numbers_in_2d_array.js
@@ -0,0 +1,20 @@
+//Return an array consisting of the largest number 
+//from each provided sub-array. For simplicity, the 
+//provided array will contain exactly 4 sub-arrays.
+
+function largestOfFour(arr) {
+  var largest = [0, 0, 0, 0];
+  for (var r=0; r < arr.length; r++) {
+     largest[r] = arr[r][0];
+    for (var c=0; c<arr[r].length; c++) {
+      if (arr[r][c] > largest[r]) {
+        largest[r] = arr[r][c];
+      }
+    }
+  }
+  return largest;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
