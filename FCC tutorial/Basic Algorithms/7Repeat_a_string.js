54a3bba23da7f70c4f7cae00cbfc6536c2f9e2da
Alexander Amundson <alex.amundson987@gmail.com>
2018-11-24 17:48:29 -0800

    Adding Repeat a string

---
+++ FCC tutorial/Basic Algorithms/7Repeat_a_string.js
@@ -0,0 +1,38 @@
+//Repeat a given string str (first argument) 
+//for num times (second argument). Return an 
+//empty string if num is not a positive number.
+
+
+//method 1: while loop
+function repeatStringNumTimes(str, num) {
+  var repeat = '';
+  while (num > 0) {
+    repeat += str;
+    num--
+  }
+  return repeat;
+}
+
+repeatStringNumTimes("abc", 3);//returns "abcabcabc"
+repeatStringNumTimes("*", 8);//returns "********"
+repeatStringNumTimes("abc", -2);//returns ""
+
+
+//method 2: recursion
+function repeatStringNumTimes(string, times) {
+  if(times < 0) 
+    return "";
+  if(times === 1) 
+    return string;
+  else 
+    return string + repeatStringNumTimes(string, --times);
+}
+
+
+//method 3: ES6 repeat function
+function repeatStringNumTimes(string, times) {
+  if (times > 0)
+    return string.repeat(times);
+  else
+    return "";
+}
