99384a1bef42ccd220b08a3e260f4b0ee666dffc
Alexander Amundson <alex.amundson987@gmail.com>
2019-03-06 20:14:58 -0500

    Sum numbers in array with for loop

--- FCC tutorial/Basic Javascript/2IterateWithForLoop.js
+++ FCC tutorial/Basic Javascript/2IterateWithForLoop.js
@@ -1,7 +1,7 @@
 //Use a for loop to work to push the values 1 through 5 onto myArray.
 
 var myArray = [];
-var thisArray = [];
+var thisArray = [ 2, 3, 4, 5, 6];
 
 for (var i = 1; i < 6; i++) {
   myArray.push(i);
@@ -20,4 +20,13 @@ for (var i=0; i < 10; i++) {
 
 for (var i = 9 ; i > 0 ; i-=2) {
    myArray.push(i);
+}
+
+//Declare and initialize a variable total to 0. 
+//Use a for loop to add the value of each element 
+//of the thisArray array to total
+
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  total += thisArr[i];
 }
