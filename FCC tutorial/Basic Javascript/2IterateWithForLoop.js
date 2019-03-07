e9fbc02079788348e60d23bc232c239245b4ddc9
Alexander Amundson <alex.amundson987@gmail.com>
2019-03-06 20:10:55 -0500

    Iterate backwards with for loop

--- FCC tutorial/Basic Javascript/2IterateWithForLoop.js
+++ FCC tutorial/Basic Javascript/2IterateWithForLoop.js
@@ -1,6 +1,7 @@
 //Use a for loop to work to push the values 1 through 5 onto myArray.
 
 var myArray = [];
+var thisArray = [];
 
 for (var i = 1; i < 6; i++) {
   myArray.push(i);
@@ -13,4 +14,10 @@ for (var i=0; i < 10; i++) {
   if ((i % 2) != 0) {
     myArray.push(i);
   }
+}
+
+//Push the odd numbers from 9 through 1 to myArray using a for loop
+
+for (var i = 9 ; i > 0 ; i-=2) {
+   myArray.push(i);
 }
