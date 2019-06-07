42b195ebb0485d727d5f9bf4074fcb551b7f8fbe
Alexander Amundson <alex.amundson987@gmail.com>
2019-06-06 21:08:22 -0400

    functionExpression

--- Codecademy tutorial/1functionExpression.js
+++ Codecademy tutorial/1functionExpression.js
@@ -10,3 +10,13 @@ const plantNeedsWater = function(day) {
 
 console.log( plantNeedsWater('Tuesday') );//returns false
 console.log( plantNeedsWater('Wednesday') );//returns true
+
+
+//Same thing but using Arrow Function
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
