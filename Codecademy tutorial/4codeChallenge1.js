eaee75e7bc0fc422c5f75236bbdcc325a8b1b59b
Alexander Amundson <alex.amundson987@gmail.com>
2019-06-20 21:26:43 -0400

    functions code challenge

--- Codecademy tutorial/4codeChallenge1.js
+++ Codecademy tutorial/4codeChallenge1.js
@@ -42,5 +42,33 @@ function truthyOrFalsy(value){
 }
 console.log(truthyOrFalsy(0.0));//returns false
 console.log(truthyOrFalsy(NaN));//returns false
-console.log(truthyOrFalsy(''"));//returns false
+console.log(truthyOrFalsy(''));//returns false
 console.log(truthyOrFalsy(false));//returns false
+
+
+/*
+Create a function, tipCalculator(), that has two parameters, a string representing the quality of the service received and a number representing the total cost.
+
+Return the tip, as a number, based on the following:
+‘bad’ should return a 5% tip
+‘ok’ should return a 15% tip
+‘good’ should return a 20% tip 
+‘excellent’ should return a 30% tip
+all other inputs should default to 18%
+*/
+
+tipCalculator = (quality, total) => {
+  switch (quality) {
+    case 'bad': return total * 0.05 ;
+      break;
+    case 'ok': return total * 0.15;
+      break;
+    case 'good': return total * 0.2;
+      break;
+    case 'excellent': return total * 0.3 ;
+    default: return total * 0.18;
+  }
+}
+console.log(tipCalculator('bad', 100));//prints 5
+console.log(tipCalculator('excellent', 100));//prints 30
+console.log(tipCalculator('decent', 100));//prints 18
