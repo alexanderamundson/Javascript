dc55d7239a0dc8fdf80de12d42cb69d277001e1b
Alexander Amundson <alex.amundson987@gmail.com>
2019-07-09 16:45:19 -0400

    string coercion

--- Misc/1stringToNumber.js
+++ Misc/1stringToNumber.js
@@ -1,3 +1,5 @@
+///////////////////////////////////////////
+//EXERCISE 1:
 //Numbers expressed as a string can be coerced into a number
 //in several ways
 
@@ -12,3 +14,23 @@ str * 1;
 parseInt(str, 10);//converts str into a base 10 decimal number
 //5
 parseFloat(str);
+
+////////////////////////////////////////////
+//EXERCISE 2
+//Extract numbers from a string
+ 
+//method 1
+let str = "This string has numbers 34 and 88";
+let final = str.split(" ").
+               filter(function(elem) {
+                       return !isNaN(elem);
+                      }).
+                map(function(val) {
+                       return parseFloat(val); 
+                });
+//alternative method 
+let str = "This string has numbers 34 and 88";
+
+let numbers = str.split(" ").
+               filter(elem => !isNaN(elem) ).
+               map(val => parseFloat(val) );
