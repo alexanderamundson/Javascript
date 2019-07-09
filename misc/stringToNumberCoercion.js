d94227a4feeb19bda22ca5e44ec2dd982e1e2fbc
Alexander Amundson <alex.amundson987@gmail.com>
2019-07-09 12:47:16 -0400

    string coercion

---
+++ misc/stringToNumberCoercion.js
@@ -0,0 +1,14 @@
+//Numbers expressed as a string can be coerced into a number in several ways
+
+let str = "98";
+//1
++str;
+//2
+str  - 0;
+//3
+str *1;
+//4
+parseInt(str, 10);
+//5
+parseFloat(str);
+
