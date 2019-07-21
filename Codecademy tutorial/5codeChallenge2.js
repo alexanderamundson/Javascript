42532dc295f43da78c04380a5c36d11966450fcc
Alexander Amundson <alex.amundson987@gmail.com>
2019-07-21 17:09:54 -0400

    code challenge 2

--- Codecademy tutorial/5codeChallenge2.js
+++ Codecademy tutorial/5codeChallenge2.js
@@ -92,3 +92,18 @@ const acceptEverything = function(stringArray) {
     console.log(`Ok, I guess I will eat some ${element}.`);
   });
 };
+
+//////////////////
+
+//Exercise 5-A
+const numbers = [2, 7, 9, 171, 52, 33, 14];
+const toSquare = num => num * num;
+const squareNums = (array) => {
+  return array.map(toSquare);
+};
+
+/*Write a function, squareNums(), that takes in an array of numbers and, 
+using .map(), returns an array with the square of each of the elements of that array*/
+
+console.log(squareNums(numbers));//returns the square of the numbers in 'numbers' array: 
+//[ 4, 49, 81, 29241, 2704, 1089, 196 ]
