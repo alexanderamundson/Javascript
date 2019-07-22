d9362f13b6455c00edc653f19f348dcf979eed05
Alexander Amundson <alex.amundson987@gmail.com>
2019-07-22 14:51:58 -0400

    code challenge 2

--- Codecademy tutorial/5codeChallenge2.js
+++ Codecademy tutorial/5codeChallenge2.js
@@ -219,3 +219,51 @@ const findMyKeys = function(array) {
 console.log(findMyKeys(randomStuff))// Should print 4
 
 
+//////////////////
+//Exercise 11
+
+/*Write a function, dogFactory(). It should:
+
+have 3 parameters: name, breed, and weight (in that order)
+expect name and breed to be strings
+expect weight to be a number
+return an object
+have each of those parameters as keys on the returned object returned with the values of the arguments that were passed in
+*/
+const dogFactory = function(name, breed, weight){ 
+  return {
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+    //Add getters and setters for each of the three properties and
+   // change the property names to have an underscore prepended.
+    get name() {
+      return this._name;
+    },
+    get breed() {
+      return this._breed;
+    },
+    get weight() {
+      return this._weight;
+    },
+    set name(name) {
+      this._name = name;
+    },
+    set breed(breed) {
+      this._breed = breed;
+    },
+    set weight(weight) {
+      this._weight = weight;
+    },
+
+    /*Add two methods to your object: .bark() which returns ‘ruff! ruff!’ 
+    and .eatTooManyTreats() which should increment the weight property by 1.*/
+    
+    bark() {
+      return 'ruff! ruff!';
+    },
+    eatTooManyTreats() {
+      this._weight++;
+    }
+  };
+}
