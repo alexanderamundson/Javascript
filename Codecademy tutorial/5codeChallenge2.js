803beb37e48b2e894c2bec8ddf7bc2df031e50fc
Alexander Amundson <alex.amundson987@gmail.com>
2019-07-22 14:41:15 -0400

    code challenge 2

--- Codecademy tutorial/5codeChallenge2.js
+++ Codecademy tutorial/5codeChallenge2.js
@@ -202,3 +202,20 @@ console.log(sortSpeciesByTeeth(speciesArray))
 //  { speciesName: 'shark', numTeeth: 50 },
 //  { speciesName: 'alligator', numTeeth: 80 } ]
 
+
+////////////////////
+//Exercise 10
+/*Write a function, findMyKeys(), that takes in an array of strings which may 
+or may not contain 'keys'. If the keys are in the array, your function should 
+return the index at which they can be found. If they’re not in the array, 
+your function should return -1*/
+
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+const findMyKeys = function(array) {
+  return array.indexOf('keys');
+}
+
+console.log(findMyKeys(randomStuff))// Should print 4
+
+
