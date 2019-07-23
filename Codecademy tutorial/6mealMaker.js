2824220ec08f824ca100de7a88fa038cd7aac40b
Alexander Amundson <alex.amundson987@gmail.com>
2019-07-23 14:11:00 -0400

    Create a random meal

--- Codecademy tutorial/6mealMaker.js
+++ Codecademy tutorial/6mealMaker.js
@@ -30,34 +30,33 @@ const menu = {
       this._courses.desserts = dessertIn;
     },   
 
-  
-  get courses() {
+    get courses() {
       return {appetizers: this._courses.appetizers,
              mains: this._courses.mains,
              desserts: this._courses.desserts};
-  },
+    },
   
-  addDishToCourse(courseName, dishName, dishPrice) {
-    const dish = {
-      name: dishName,
-      price: dishPrice
-    };
+    addDishToCourse(courseName, dishName, dishPrice) {
+      const dish = {
+        name: dishName,
+        price: dishPrice
+      };
     
-    this._courses[courseName].push(dish);
-  },
+      this._courses[courseName].push(dish);
+    },
   
-  getRandomDishFromCourse(courseName) {
-    const dishes = this._courses[courseName];
-    return dishes[Math.floor(Math.random() * dishes.length)];
-  },
+    getRandomDishFromCourse(courseName) {
+      const dishes = this._courses[courseName];
+      return dishes[Math.floor(Math.random() * dishes.length)];
+    },
   
-  generateRandomMeal(){
-    const appetizer = this.getRandomDishFromCourse('appetizers');
-    const main = this.getRandomDishFromCourse('mains');
-    const dessert = this.getRandomDishFromCourse('desserts');
-    const totalPrice = appetizer.price + main.price + dessert.price;
-    return `Your meal is ${appetizer.name}, ${main.name}, ${dessert.name} ... The price is $${totalPrice.toFixed(2)}.`;  
-  }
+    generateRandomMeal(){
+      const appetizer = this.getRandomDishFromCourse('appetizers');
+      const main = this.getRandomDishFromCourse('mains');
+      const dessert = this.getRandomDishFromCourse('desserts');
+      const totalPrice = appetizer.price + main.price + dessert.price;
+      return `Your meal is ${appetizer.name}, ${main.name}, ${dessert.name} ... The price is $${totalPrice.toFixed(2)}.`;  
+   }
 };
 
 //fill the menu with dishes and their price
@@ -72,6 +71,6 @@ menu.addDishToCourse('desserts', 'Watermelon', 1.25);
 menu.addDishToCourse('desserts', 'Mamey Sapote', 3.00);
 
 
-//generate a randomly selected meal
+//Display a randomly selected meal and the total price
 const meal = menu.generateRandomMeal();
 console.log(meal);
