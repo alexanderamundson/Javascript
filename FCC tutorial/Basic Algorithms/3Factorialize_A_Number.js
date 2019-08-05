94db9ce4e8f2c890c5598c4a67e1b02e1fdb3dc5
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-05 17:53:30 -0400

    returns factorial of input

--- FCC tutorial/Basic Algorithms/3Factorialize_A_Number.js
+++ FCC tutorial/Basic Algorithms/3Factorialize_A_Number.js
@@ -1,6 +1,11 @@
 function factorialize(num) {
+  if (num < 0) {
+    return;
+  }
   if (num === 0 || num === 1) {
     return 1;
   }
-  return num * factorialize(num -1); 
+  return num * factorialize(--num);
 }
+
+factorialize(-5);
