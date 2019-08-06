5d1542bcf4fcecdf266eabc8e246edf65d70bac5
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-06 19:05:39 -0400

    Refactored solution

--- FCC tutorial/Basic Algorithms/6Confirm_ending_string.js
+++ FCC tutorial/Basic Algorithms/6Confirm_ending_string.js
@@ -1,16 +1,11 @@
-//Check if a string (first argument, str) ends with 
-//the given target string (second argument, target)
+//Check if a string (first argument, str) ends with the given target string (second argument, target).
 
 function confirmEnding(str, target) {
-  var stringLength = str.length;
-  var targetLength = target.length;
-  var ending = str.substring(stringLength - targetLength);
-  return ending === target ? true : false;
-  
-  //Same code condensed to single line:
-  //return str.substring(str.length - target.length) === target ? true: false;
+  var ending;
+  var endingStartingIndex = str.length - target.length;
+  ending = (str.substring(endingStartingIndex));
+  return ending === target;
 }
 
 confirmEnding("Bastian", "n");//returns true
-confirmEnding("Abstraction", "action");//returns true
-confirmEnding("Connor", "n")//returns false
+confirmEnding("Connor", "n");// returns false.
