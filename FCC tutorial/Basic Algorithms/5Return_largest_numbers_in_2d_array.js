48acb1477500e4f03e36c74d0f8b14f0586ecfd4
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-06 18:48:09 -0400

    Added another solution

--- FCC tutorial/Basic Algorithms/5Return_largest_numbers_in_2d_array.js
+++ FCC tutorial/Basic Algorithms/5Return_largest_numbers_in_2d_array.js
@@ -15,10 +15,28 @@ function largestOfFour(arr) {
   }
   return largest;
 }
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);// returns [5, 27, 39, 1001]
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
-//Method 2: map method and apply Math.max
+//Method 2: Array sort method
+function largestOfFour(arr) {
+  let maxArray = [];
+  //loop through the array
+    for (let i=0; i<arr.length; i++) {
+    //call Array.prototype.sort on each sub-array
+    arr[i].sort(function(elem1, elem2){
+       return elem2 - elem1;//sorts array descending
+    });
+    //push larget number in each subarray into maxArray
+    maxArray.push(arr[i][0]);
+    }
+  return maxArray;
+}
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);// should return [25, 48, 21, -3]
+
+
+
+//Method 3: map method and apply Math.max
 function largestOfFour(arr) {
     return arr.map(function (index) {
       return Math.max.apply(null, index);
