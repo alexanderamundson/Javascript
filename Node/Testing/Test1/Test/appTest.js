18b98b29edee4305774e03dc22f02b62eb5445d2
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-12 13:44:37 -0400

    Ensure sayHello returns string

--- Node/Testing/Test1/Test/appTest.js
+++ Node/Testing/Test1/Test/appTest.js
@@ -3,9 +3,14 @@ const sayHello = require('../App/app').sayHello;
 //const app = require('../App/app');
 
 
-describe('App', function(){
-    it('App should return Hello World', function(){
+describe('App.sayHello', function(){
+    it('SayHello() should return Hello World', function(){
         let result = sayHello();
         assert.equal(result, 'Hello World');
+    });
+
+    it('sayHello() should return type string', function(){
+        let result = sayHello();
+        assert.typeOf(result, 'string');
     });
 });
