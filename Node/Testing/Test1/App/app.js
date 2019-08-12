033db7c7ffbd344355352ee39cd8771094e74f1b
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-12 13:55:22 -0400

    First Chai assertion test

--- Node/Testing/Test1/App/app.js
+++ Node/Testing/Test1/App/app.js
@@ -1,5 +1,8 @@
 module.exports = {
     sayHello: function(){
         return 'Hello World';
+    },
+    addNumbers: function(num1, num2) {
+        return num1 + num2;
     }
 }
--- Node/Testing/Test1/Test/appTest.js
+++ Node/Testing/Test1/Test/appTest.js
@@ -1,16 +1,25 @@
 const assert = require('chai').assert;
-const sayHello = require('../App/app').sayHello;
-//const app = require('../App/app');
+const app = require('../App/app');
 
 
 describe('App.sayHello', function(){
     it('SayHello() should return Hello World', function(){
-        let result = sayHello();
+        let result = app.sayHello();
         assert.equal(result, 'Hello World');
     });
 
     it('sayHello() should return type string', function(){
-        let result = sayHello();
+        let result = app.sayHello();
         assert.typeOf(result, 'string');
+    });
+
+    it('AddNumbers() should be == 10 when both inputs are 5', function() {
+        let result = app.addNumbers(5, 5);
+        assert.equal(result, 10);
+    });
+
+    it('addNumbers() should return type number', function() {
+        let result = app.addNumbers(5, 5);
+        assert.typeOf(result, 'number');
     });
 });
