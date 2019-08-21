21e4cd2176b61cad320b50d7147861bc4e355eec
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-21 17:45:34 -0400

    JavaScript Regular Expressions

--- FCC tutorial/Regex/1Regex.js
+++ FCC tutorial/Regex/1Regex.js
@@ -35,3 +35,40 @@ let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; 
 let result = petRegex.test(petString);
 console.log(result);//displays true
+
+//4.
+/*
+The flag that ignores case is the i flag. 
+You can use it by appending it to the regex. 
+An example of using this flag is /ignorecase/i.
+*/
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i;
+let result = fccRegex.test(myString);
+console.log(result);//displays true
+
+//5.
+/*
+You can extract the regex pattern matches you found 
+with the .match() method. To use the .match() method, 
+apply the method on a string and pass in the regex 
+inside the parentheses.
+*/
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/;
+let result = extractStr.match(codingRegex); 
+console.log(result);//displays 'coding'
+
+//6.
+/*
+The "g" flag indicates that the regular expression should 
+be tested against all possible matches in a string.
+Without the g flag, it'll only test for the first match.
+*/ 
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/ig; //used to search for all matches of the string twinkle ignoring case
+let result = twinkleStar.match(starRegex);
+console.log(result);//displays 'Twinkle,twinkle'
