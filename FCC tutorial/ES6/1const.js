f80bedbc17596ce1042c169aa7defc96682257b6
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-21 14:49:28 -0400

    const in ES6

--- FCC tutorial/ES6/1const.js
+++ FCC tutorial/ES6/1const.js
@@ -37,3 +37,28 @@ function printManyTimes(str) {
    
  }
  editInPlace();
+
+
+ //3.
+
+ /*
+ JavaScript provides a function Object.freeze to prevent data mutation of objects.
+Once the object is frozen, you can no longer add, update, or delete 
+properties from it. Any attempt at changing the object will be rejected without an error
+ */
+
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  Object.freeze(MATH_CONSTANTS);
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+console.log(PI);
