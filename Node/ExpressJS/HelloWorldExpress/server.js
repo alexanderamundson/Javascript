d27e6431de1a07e5af7a676b28ff621958c11cfa
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-24 19:43:58 -0400

    add pug view template

--- Node/ExpressJS/HelloWorldExpress/server.js
+++ Node/ExpressJS/HelloWorldExpress/server.js
@@ -5,6 +5,10 @@ app.set('view engine', 'pug');
 app.set('views','./views');
 
 
+
+
+
+
 //static routes
 app.get("/", (req, res)=>{
     res.send("Hello World!!!");
@@ -12,10 +16,24 @@ app.get("/", (req, res)=>{
 
 //pug template route
 app.get("/pug", (req,res) => {
-   res.render("index.pug");
-})
+   res.render("hello_pug.pug");
+});
+
+app.get('/partials', function(req, res){
+   res.render('content');
+});
 
 /* Dynamic Routes */
+//dynamic pug view
+ app.get('/dynamic_view', function(req, res){
+  res.render('dynamic', {
+     name: "MyGithub", 
+     url:"https://github.com/alexanderamundson"
+  });
+});
+
+
+
 //dynamic route (accepts any id)
 app.get('/:id', function(req, res){
     res.send('The id you specified is ' + req.params.id);
@@ -32,8 +50,9 @@ app.get('/:id', function(req, res){
  });
 
 
-/*Special Routes */
 
+
+/*Special Routes */
 //'all' ignores the http method
 app.all('/test', (req, res)=>{
     res.send("the 'all' method works the same regardless of the HTTP method type ----- e.g:"+
