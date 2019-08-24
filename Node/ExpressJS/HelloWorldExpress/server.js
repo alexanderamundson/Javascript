50a670c426c0708b63fe0952f73169dc752cfa78
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-23 20:36:21 -0400

    Added dynamic route

--- Node/ExpressJS/HelloWorldExpress/server.js
+++ Node/ExpressJS/HelloWorldExpress/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+//static route
 app.get("/", (req, res)=>{
     res.send("Hello World!!!");
 });
 
+//dynamic route (accepts any id)
+app.get('/:id', function(req, res){
+    res.send('The id you specified is ' + req.params.id);
+ });
+
+//'all' ignores the http method
+app.all('/test', (req, res)=>{
+    res.send("the 'all' method works the same regardless of the HTTP method type ----- e.g:"+
+              "app.all('/test', (req, res)=>{ ...");
+ });
 app.listen("8080");
