7411a000bf368d486cbc3c0d972a9e76a03c7589
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-27 14:02:58 -0400

    added new route

--- Node/ExpressJS/2ExpressPractice/index.js
+++ Node/ExpressJS/2ExpressPractice/index.js
@@ -10,8 +10,19 @@ const app = express();
 app.use(logger);
 
 
-//send json to browser
+//send json of members to browser
 app.get('/api/members', (req, res) => res.json(members));
+
+//get specific member by id
+app.get('/api/members/:id', (req, res) => {
+    const exists = members.some(member => member.id === parseInt(req.params.id) );
+
+    if (exists) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    }else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id} exists` });
+    }
+});
 
 
 
