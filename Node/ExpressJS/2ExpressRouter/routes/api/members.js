06af7984ce923914eec557ad16d2740a46192d58
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-27 19:23:23 -0400

    CRUD API

--- Node/ExpressJS/2ExpressRouter/routes/api/members.js
+++ Node/ExpressJS/2ExpressRouter/routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uuid = require('uuid');
 const router = express.Router();
 const members = require('../../Members');
 
@@ -15,5 +16,56 @@ router.get('/:id', (req, res) => {
         res.status(400).json({ msg: `No member with the id of ${req.params.id} exists` });
     }
 });
+
+// Create new Member
+router.post('/', (req, res) => {
+    const newMember = {
+      id: uuid.v4(),
+      name: req.body.name,
+      email: req.body.email,
+      status: 'active'
+    };
+  
+    if (!newMember.name || !newMember.email) {
+      return res.status(400).json({ msg: 'Please include a name and email for a new member' });
+    }
+    members.push(newMember);
+    res.json(members);
+    // res.redirect('/');
+});
+
+
+// Update Member
+router.put('/:id', (req, res) => {
+    const exists = members.some(member => member.id === parseInt(req.params.id));
+  
+    if (exists) {
+      const memberUpdates = req.body;
+      members.forEach(member => {
+        if (member.id === parseInt(req.params.id)) {
+          member.name = memberUpdates.name ? memberUpdates.name : member.name;
+          member.email = memberUpdates.email ? memberUpdates.email : member.email;
+  
+          res.json({ msg: 'Member updated', member });
+        }
+      });
+    } else {
+      res.status(400).json({ msg: `No member with the id of ${req.params.id} exists` });
+    }
+  });
+
+  // Delete Member
+router.delete('/:id', (req, res) => {
+    const exists = members.some(member => member.id === parseInt(req.params.id));
+  
+    if (exists) {
+      res.json({
+        msg: 'Member deleted',
+        members: members.filter(member => member.id !== parseInt(req.params.id))
+      });
+    } else {
+      res.status(400).json({ msg: `No member with the id of ${req.params.id} exists` });
+    }
+  });
 
 module.exports = router;
