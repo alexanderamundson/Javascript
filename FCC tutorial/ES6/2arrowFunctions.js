108ea4b8a128be37c11a43caf49eabed330e7787
Alexander Amundson <alex.amundson987@gmail.com>
2019-09-23 22:27:39 -0400

    arrow functions ES6

--- FCC tutorial/ES6/2arrowFunctions.js
+++ FCC tutorial/ES6/2arrowFunctions.js
@@ -36,9 +36,13 @@ console.log(myConcat([1, 2], [3, 4, 5]));
 //displays 1,2,3,4,5
 
 
-//4.
+//4.  solution 1 (using parseInt on line )
 /*
-arrow functions work well with higher order functions
+arrow functions work well with higher order functions..
+Use arrow function syntax to compute the square of only 
+the positive integers (decimal numbers are not integers) 
+in the array realNumberArrayand store the new array in the 
+variable squaredIntegers
 */
 
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
@@ -56,3 +60,31 @@ const squareList = (arr) => {
 const squaredIntegers = squareList(realNumberArray);
 console.log(squaredIntegers);
 
+//4. solution 2 (using Number.isInteger on line  71)
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+
+  const squaredIntegers = arr.filter(
+    (elem) => {
+         return elem > 0 && Number.isInteger(elem);
+    }).map(
+      (num) =>{
+         return  num * num;
+    });
+  
+  console.log(squaredIntegers);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers); 
+
+
+//5.
+
+//6.
+
+//7.
