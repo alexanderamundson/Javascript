b2305e89314bf0e1d357448b1b5f788fe50bc426
Alexander Amundson <alex.amundson987@gmail.com>
2019-09-25 13:37:07 -0400

    nested object deconstruction ES6

--- FCC tutorial/ES6/4destructuring.js
+++ FCC tutorial/ES6/4destructuring.js
@@ -39,8 +39,16 @@ console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
 
 //3.
 /*
+Use destructuring assignment to swap the values of a and b so that a
+receives the value stored in b, and b receives the value stored in a
  */
-
+let a = 8, b = 6;
+(() => {
+  "use strict";
+   [b,a] = [a,b]
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
 
 
  //4.
