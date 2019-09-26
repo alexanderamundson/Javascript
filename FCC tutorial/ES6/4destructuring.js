df95389c6f2967025b0ad8d0e939a8bf89308ea9
Alexander Amundson <alex.amundson987@gmail.com>
2019-09-25 20:34:30 -0400

    function argument deconstruction ES6

--- FCC tutorial/ES6/4destructuring.js
+++ FCC tutorial/ES6/4destructuring.js
@@ -89,5 +89,25 @@ console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
 
 
 //5.
+/*
+You can destructure the object in a function argument itself.
+*/
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function() {
+  "use strict";    
+   return function half( {max, min} ) {//only sends stats.max and stats.min
+     return (max + min) / 2.0;
+};
+})();
+  
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
 
 //6.
