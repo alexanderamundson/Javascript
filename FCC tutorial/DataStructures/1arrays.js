cc394ae3d9e8acae70a9f412c5aa7cb0eeeb83e8
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-10 18:17:23 -0400

    JavaScript array push, pop, shift, unshift methods

---
+++ FCC tutorial/DataStructures/1arrays.js
@@ -0,0 +1,40 @@
+//1.
+/*
+Arrays are a linear data structure whose elements are accesed by indices.
+Array indices start at 0.  Arrays have a length property that returns the
+number of elements in the array.
+*/
+var arrayName = [1, 3, 4, 6];
+console.log(arrayName[0]);//displays 1
+console.log(arrayName.length);//displays 4
+
+//2
+/*
+An element or a series of elements can be added to an array using the
+push or unshift methods. push adds element/s to the end of the array. unshift 
+adds element/s to the end of the array.
+ */
+var array = [1,2,3];
+array.push(4);
+array.unshift(0);
+console.log(array);//displays 0, 1, 2, 3, 4
+
+//3
+/*
+An element or a series of elements can be removed an array using the
+pop or shift methods. pop removes an element from the end of the array. unshift 
+removes the first element in the array.
+ */
+var arr = [1,2,3];
+arr.pop();//removes 3
+arr.shift();//removes 1
+console.log(array);//displays 2
+
+//4
+
+
+//5
+
+//6
+
+//7
