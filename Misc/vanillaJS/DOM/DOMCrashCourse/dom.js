98fe1b95dfbf7bf4593f0328b5702c5d966945e7
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-10 13:22:03 -0400

    event list with search filter

--- Misc/vanillaJS/DOM/DOMCrashCourse/dom.js
+++ Misc/vanillaJS/DOM/DOMCrashCourse/dom.js
@@ -6,10 +6,16 @@ form.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 filter.addEventListener("keyup", filterItems);
 
+
+
 function addItem(event) {
     event.preventDefault();
     //get input value
     let newItem = document.getElementById("item").value;
+    if (!newItem) { //if user submits empty string as item
+        alert('Can not submit empty item');
+        return;
+    }
     //create new li
     let li = document.createElement('li');
     li.className = 'list-group-item';
@@ -27,8 +33,7 @@ function addItem(event) {
     itemList.appendChild(li);
 }
 
-function removeItem(event) {
-    
+function removeItem(event) {   
     if (event.target.classList.contains('delete')) {
         if (confirm("Are You Sure?")) {
             let li = event.target.parentElement;
@@ -36,7 +41,6 @@ function removeItem(event) {
         }
     }
 };
-
 
 function filterItems(event) {
     //convert input to lowercase
@@ -46,131 +50,11 @@ function filterItems(event) {
     //convert items html collection to array
     Array.from(items).forEach(function(item){
         let itemName = item.firstChild.textContent;
+        console.log(`li display = ${item.style.display}*`);
         if (itemName.toLowerCase().indexOf(text) != -1) {
             item.style.display = 'block';
         } else {
-            item.style.display = 'none';
+            item.style.display = 'none';//Hide elements that don't match 'text'
         }
-    })
+    });
 }
-
-/*
-const itemList = document.getElementById("items");
-const itemList = document.getElementById("items");
-const itemList = document.getElementById("items");
-const itemList = document.getElementById("items");
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// EXAMINE THE DOCUMENT OBJECT //
-
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// //document.title =  123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[10]);
-// // document.all[10].textContent = 'Hello';
-// console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
-
-// GETELEMENTBYID //
-// console.log(document.getElementById('header-title'));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Goodbye';
-// console.log(headerTitle.innerText);
-// headerTitle.innerHTML = '<h3>Hello</h3>';
-// header.style.borderBottom = 'solid 3px #000';
-
-// GETELEMENTSBYCLASSNAME //
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
-
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(var i = 0; i < items.length; i++){
-//   items[i].style.backgroundColor = '#f4f4f4';
-// }
-
-// GETELEMENTSBYTAGNAME //
-// var li = document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent = 'Hello 2';
-// li[1].style.fontWeight = 'bold';
-// li[1].style.backgroundColor = 'yellow';
-
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(var i = 0; i < li.length; i++){
-//   li[i].style.backgroundColor = '#f4f4f4';
-// }
-
-// QUERYSELECTOR //
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #ccc';
-
-// var input = document.querySelector('input');
-// input.value = 'Hello World'
-
-// var submit = document.querySelector('input[type="submit"]');
-// submit.value="SEND"
-
-// var item = document.querySelector('.list-group-item');
-// item.style.color = 'red';
-
-// var lastItem = document.querySelector('.list-group-item:last-child');
-// lastItem.style.color = 'blue';
-
-// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.color = 'coral';
-
-// QUERYSELECTORALL //
-// var titles = document.querySelectorAll('.title');
-
-// console.log(titles);
-// titles[0].textContent = 'Hello';
-
-// var odd = document.querySelectorAll('li:nth-child(odd)');
-// var even= document.querySelectorAll('li:nth-child(even)');
-
-// for(var i = 0; i < odd.length; i++){
-//   odd[i].style.backgroundColor = '#f4f4f4';
-//   even[i].style.backgroundColor = '#ccc';
-// }
-
