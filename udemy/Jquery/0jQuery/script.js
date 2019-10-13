db6c28956f351492d91a33c6567d3658566416ac
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-13 17:10:55 -0400

    Practcing jQueery

--- udemy/Jquery/0jQuery/script.js
+++ udemy/Jquery/0jQuery/script.js
@@ -1,16 +1,33 @@
 const element = $("header")[0];
+const div1 = $('#div1');
+const div2 = $('#div2');
+const div3 = $('#div3');
+const button1 = $('#button1');
+const button2 = $('#button2');
 
 
 element.addEventListener("click", function(){
     $("header").css("background", "silver");
-    $("header").html(`<div>this  ${this} is a div</div>`);
 });
 
 element.addEventListener("mouseover", function(){
     $("header").css("fontSize", "45px");
+    $("header").text("Click ME");
 });
 
-$("button").dblclick(function() {
+button1.click(function() {
+    alert("clicked button 1");
+    div1.css(
+        {"background": "gray",
+         "fontSize":   "1.5em",
+         "color": "blue"
+        });
+});
+
+
+button2.dblclick( function() {
+    alert("clicked button 2");
     $(this).css("width", "180px");
     $(this).css("border: 5px solid green");
 });
+
