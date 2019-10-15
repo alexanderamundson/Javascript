cc5683c906d1406c01ae8cc9144b892f8b2a1b59
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-14 22:45:01 -0400

    convert string to pig latin

---
+++ FCC tutorial/Algorithms/Intermediate/6pigLatin.js
@@ -0,0 +1,30 @@
+/*
+Translate the provided string to pig latin.
+Pig Latin takes the first consonant (or consonant cluster) of an English word,
+moves it to the end of the word and suffixes an "ay".
+If a word begins with a vowel, simply add "way" to the end.
+Input strings are all lowercase.
+*/
+
+function translatePigLatin(str) {
+    //if begins with vowel, just add way
+    if (str.startsWith("a") || str.startsWith("e") ||str.startsWith("i") || str.startsWith("o") || str.startsWith("u")) {
+     return str + "way"; 
+    }
+    //else find the begining constanant/s and index of first vowel
+    let consCluster = "";//is the starting constanant/s
+    let firstVowelIndex = 0;//index of first vowel in str
+    
+    for (let i =0; i < str.length; i++) {
+     if (str.charAt(i) != "a" && str.charAt(i) != "e" && str.charAt(i) != "i" && str.charAt(i) != "o" && str.charAt(i) != "u") {
+       continue;
+     } else {
+       consCluster = str.substring(0, i);
+       firstVowelIndex = i;
+       break;
+     }
+    }
+    return str.substring(firstVowelIndex) + consCluster +  "ay";
+  }
+  
+  translatePigLatin("glove");
