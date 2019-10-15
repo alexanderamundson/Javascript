d1e5a921c6cea93ab6b148e990940946db6c84f4
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-14 20:49:03 -0400

    javascript object practice

---
+++ FCC tutorial/DataStructures/2objects.js
@@ -0,0 +1,14 @@
+//1 Adding object properties
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+  
+//You can add properties using dot notation
+//or with bracket notation.
+foods.bananas = 13;
+foods.grapes = 35;
+foods["strawberries"] = 27;
+  
+console.log(foods);
