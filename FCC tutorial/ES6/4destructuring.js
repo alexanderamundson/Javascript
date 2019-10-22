2e7f398d3b7feec7e0822152d55ec778f0a35064
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-21 22:44:13 -0400

    ES6 destructuring assignments

--- FCC tutorial/ES6/4destructuring.js
+++ FCC tutorial/ES6/4destructuring.js
@@ -111,3 +111,17 @@ console.log(stats); // should be object
 console.log(half(stats)); // should be 28.015
 
 //6.
+/*Replace the two assignments with an equivalent destructuring assignment. 
+It should still assign the variables today and tomorrow the values of today
+and tomorrow from the HIGH_TEMPERATURES object.*/
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+const { today, tomorrow} = HIGH_TEMPERATURES;
+
+console.log(yesterday) // should be not defined
+console.log(today); // should be 77
+console.log(tomorrow); // should be 80
