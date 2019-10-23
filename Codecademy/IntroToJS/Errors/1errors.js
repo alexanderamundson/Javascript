2929e4625ca39288f5c9a79a7bd9c101b2efa1d7
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-22 22:54:02 -0400

    Throwing Error object

--- Codecademy/IntroToJS/Errors/1errors.js
+++ Codecademy/IntroToJS/Errors/1errors.js
@@ -1,4 +1,5 @@
-/*Creating an error 
+/*
+1. Creating an error 
 is not the same as throwing an error. A thrown error will cause the 
 program to stop running.
 */
@@ -6,3 +7,11 @@ console.log(new Error('User missing name'));/*displays the error
 but does not stop the program
 */
 console.log('logging the error above wont stop our program from running.');
+
+
+//2.
+/*
+An error must be thrown for it to halt the program.
+*/
+//this will halt the program 
+throw new Error('Username or password do not match');
