bbce33e2970cf6cc99b62346a724f3b7f71a8d96
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-22 22:50:56 -0400

    Creating Error object

---
+++ Codecademy/IntroToJS/Errors/1errors.js
@@ -0,0 +1,8 @@
+/*Creating an error 
+is not the same as throwing an error. A thrown error will cause the 
+program to stop running.
+*/
+console.log(new Error('User missing name'));/*displays the error
+but does not stop the program
+*/
+console.log('logging the error above wont stop our program from running.');
