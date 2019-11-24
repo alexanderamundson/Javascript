eac7f478e6366997701fe471027c7bfce470fc68
Alexander Amundson <alex.amundson987@gmail.com>
2019-11-23 19:31:49 -0500

    Converts multidimensional array into 1-D array

--- FCC tutorial/Algorithms/Intermediate/15decomposeArray.js
+++ FCC tutorial/Algorithms/Intermediate/15decomposeArray.js
@@ -1,9 +1,9 @@
+//Converts multidimensional array into a single array
 function steamrollArray(arr) {
     while (arr.some(Array.isArray)) 
-      arr = [].concat(...arr);
-  
+      arr = [].concat(...arr); 
       return a;
-  }
+}
   
-  steamrollArray([1, [2], [3, [[4]]]]);//should return [1,2,3,4] 
+steamrollArray([1, [2], [3, [[4]]]]);//should return [1,2,3,4] 
   
