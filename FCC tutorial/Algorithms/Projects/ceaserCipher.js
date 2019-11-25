3da72a471cbe76ad0e1ae2552ea4e0e74b87da82
Alexander Amundson <alex.amundson987@gmail.com>
2019-11-25 14:14:20 -0500

    Converts ciphertext to plaintext with ROT13
    Uses caeser cipher ROT13

--- FCC tutorial/Algorithms/Projects/ceaserCipher.js
+++ FCC tutorial/Algorithms/Projects/ceaserCipher.js
@@ -1,62 +1,59 @@
-function rot13(str) { // LBH QVQ VG!
-    //split str to array of chars
-    //
-    str=str.split("");
-    var ROT13 = str.map(toPlainText);
-   
-   //getCharCode
-    // check if non-letter
-    //given a charCode, return shifted by 13 char
-    function toPlainText(char) {
+/*The ROT13 caeser cipher, shifts the values of the letters 13 places. 
+Thus 'A' becomes 'N', 'B' becomes 'O' and so on.
 
-      //console.log(char + " to charCode: " + char.charCodeAt(0));
+This function takes a ROT13 encoded string as input 
+and returns a decoded plaintext string.
+*/
+function rot13(str) { 
+
+    //converts each character in 'str' to plain text
+    return str.split("").map(toPlainText).join("");
+    
+    
+    /*Converts ciphered parameter to plain text */
+    function toPlainText(char) {
       var charCode = char.charCodeAt(0);
       var plainText;
-      //65-90-97-122
       if ( isLetter(charCode) ) {
-        plainText = cipher(charCode);
-        ////////console.log("plainText: "+ plainText);
+        plainText = decipher(charCode);
         return plainText;
       }
-      return plainText = char;
+      return plainText = char;//if char is just a space character, simply return it
     };
    
-   
+    /*Returns true if 'charCode' is a letter */
     function isLetter(charCode)  {
       return ( (charCode >= 65 && charCode <= 90) ||
                (charCode >= 97 && charCode <= 122)   ) 
     };
    
-   
-   
-   function cipher(charCode) {
-      ///////////// console.log(charCode + " is upper? " + isUpperCase(charCode));
+   /*Shifts charCode by 13, and returns ROT13 character */
+   function decipher(charCode) {
      if (isUpperCase(charCode)) {
-       //getNewCharCode
-       var newCharCode = getNewCharCode(charCode, "upper");
-       //charCode + 13  <= 122
+       var newCharCode = getNewCharCode(charCode, "upper");   
      } else {
        var newCharCode = getNewCharCode(charCode, "lower");
      }
-     return String.fromCharCode(newCharCode);
+     return String.fromCharCode(newCharCode);//converts new CharCode to Character value 
    };
    
+   /*Determines if 'charCode' is an uppercase character */
    function isUpperCase(charCode) {
     return charCode >= 65 && charCode <= 90;
    };
    
+   /*shifts the charCode by 13, returns char code of plain text character */
    function getNewCharCode(charCode, letterCase) {
-    
      var newCharCode;
-     if (letterCase === "lower") {
+     if (letterCase === "lower") {//if 'charcode' is lower case
        if (charCode + 13 > 122) {
         newCharCode = ((charCode + 13) % 122 ) + 96;
        }  else {
          newCharCode = charCode + 13;
        }
     
-     } //UPPER case
-      else {
+     }
+     else {//if 'charCode' is uppercase
         if (charCode + 13 > 90) {
           newCharCode = ((charCode +  13) % 90) + 64;
         } else {
@@ -66,8 +63,7 @@ function rot13(str) { // LBH QVQ VG!
      return newCharCode;
    };
 
-   return ROT13.join("");
-}//end rot13
+}//end rot13 function
    
 
    rot13("SERR PBQR PNZC");
